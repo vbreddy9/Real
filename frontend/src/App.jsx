0e44a4e161c36560994ddf371ea3b011baf7e051
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-05-21 11:06:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import PrivacyPolicy from "./PrivacyPolicy";
@@ -11,8 +16,53 @@ const AppContent = () => {
   const hideNavbarOn = ["/privacy-policy", "/terms-conditions"];
   const shouldHideNavbar = hideNavbarOn.includes(location.pathname);
 
+  useEffect(() => {
+    // Load Meta Pixel script once
+    if (!window.fbq) {
+      !(function (f, b, e, v, n, t, s) {
+        if (f.fbq) return;
+        n = f.fbq = function () {
+          n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments);
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = true;
+        n.version = "2.0";
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = true;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s);
+      })(
+        window,
+        document,
+        "script",
+        "https://connect.facebook.net/en_US/fbevents.js"
+      );
+
+      window.fbq("init", "1256259199226528");
+    }
+
+    // Track page view on route change
+    window.fbq("track", "PageView");
+  }, [location.pathname]); // Track on every route change
+
   return (
     <>
+      {/* Meta Pixel noscript fallback */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1256259199226528&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+
       {!shouldHideNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
